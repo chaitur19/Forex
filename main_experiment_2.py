1a88884ee0c59009465e93c7e2c30827f3603a28
chaitur19 <chaitur19@gmail.com>
2023-04-22 03:32:08 -0400

    Update main_experiment_2.py

--- main_experiment_2.py
+++ main_experiment_2.py
@@ -99,7 +99,7 @@ def sentiment_analyis_prediction(currencypair, currency_pair_list):
     for i in searchList:
         
         #resp=driver.get('https://www.google.com/search?q='+i+'&authuser=0&tbm=nws&sxsrf=APwXEddvFjyX-E3Jv6SMNq5CWpM-X9jFSg:1681868177846&ei=kUU_ZJOXM8_cptQPkoOQmA8&start=20&sa=N&ved=2ahUKEwjTo7Ka57T-AhVProkEHZIBBPM4ChDy0wN6BAgFEAc&biw=1036&bih=909&dpr=1.02')
-        resp=driver.get('http://www.google.com')
+        resp=driver.get('http://www.example.com')
         dv = driver.find_elements("xpath", '//div[@class="SoaBEf"]')
         for element in dv:
             title= element.find_element("xpath", './/div[@class="mCBkyc ynAwRc MBeuO nDgy9d"]')
