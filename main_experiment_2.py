7c4261793c1cf733f70a7d34ae8ff8392a268cc9
chaitur19 <chaitur19@gmail.com>
2023-04-22 03:53:58 -0400

    Update main_experiment_2.py

--- main_experiment_2.py
+++ main_experiment_2.py
@@ -22,6 +22,6 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-driver.get("http://example.com")
+driver.get("http://google.com")
 
 #st.code(driver.page_source)
